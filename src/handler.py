b940785f953182595806019a45d1a661b933207d
HeltonAL <HeltonAL@etsu.edu>
2021-01-31 16:02:14 -0500

    Code refactoring and updating

--- src/handler.py
+++ src/handler.py
@@ -15,58 +15,59 @@ db_name = os.environ.get("MONGO_DB_NAME", "new_hire_test")
 db = MongoClient(db_uri)[db_name]
 
 
-def process_hire_date(date, response_body):
+def process_hire_date(date):
     '''
         Tries to create a hire date using parse from dateutil.parse
     '''
     if date == "":
         return date
 
-    hire_date = None
+    _date = None
     # Just catching a general exception since it is due to bad input
     try:
-        hire_date = parse(date.strip())
+        _date = parse(date.strip())
     except(Exception):
         # This would normally be logged
         response_body["errors"].append(f"{date}: is not a valid date")
         return None
 
-    return hire_date
+    return _date
 
 
-def process_salary(value, response_body):
+def process_salary(salary):
     '''
         Tries to create a salary based on the passed value
     '''
-    salary = None
+    _salary = None
     try:
-        if isinstance(value, str):
-            salary = value.strip()
-            if salary == "":
+        if isinstance(salary, str):
+            _salary = salary.strip()
+            if _salary == "":
                 return ""
-            salary = int(salary)
+            _salary = int(_salary)
         # Since everything is from a CSV the two cases below should not be
         # possible but are other accounting for if the input ever changed
-        elif isinstance(value, int):
-            return salary
+        elif isinstance(salary, int):
+            return _salary
         else:
-            salary = int(value)
+            _salary = int(salary)
     except(Exception):
-        response_body["errors"].append(f"{value}: is not a valid salary")
+        response_body["errors"].append(f"{salary}: is not a valid salary")
         return None
 
-    return salary
+    return _salary
 
 
-def process_manager_id(manager_email, response_body):
+def process_manager_id(email):
     '''
-        Checks to see if manager_email is a valid email and if a user
-        exists with an email the same as manager_email.
-        Returns either the Id of the user object with manager_email
-        or None because the data is invalid or the user doesn't exist
+        Checks to see if email is a valid email and if a user
+        exists with the same email.
+        Returns either the Id of the user object with email the
+        associated email or None because the data is invalid
+        or the user doesn't exist
     '''
     manager_id = None
-    manager_email = validate_email(manager_email)
+    manager_email = validate_email(email)
 
     if manager_email is None:
         response_body["errors"].append(f"{manager_email}: is not a valid "
@@ -82,21 +83,21 @@ def process_manager_id(manager_email, response_body):
     return manager_id
 
 
-def validate_user_email(user_email, response_body):
+def validate_user_email(email):
     '''
         This function is here for user email error checking without
         adding the logic to validate_email which is used by other functions
         and wouldn't be correct for them.
     '''
-    email = validate_email(user_email)
+    _email = validate_email(email)
 
-    if email is None:
-        response_body["errors"].append(f"{user_email}: is not "
+    if _email is None:
+        response_body["errors"].append(f"{email}: is not "
                                        + "a user valid email")
-    elif email == "":
+    elif _email == "":
         response_body["errors"].append("user email was blank")
 
-    return email
+    return _email
 
 
 def validate_email(email):
@@ -118,23 +119,24 @@ def validate_email(email):
     return None
 
 
-def validate_name(value, response_body):
+def validate_name(name):
     '''
         Verifies if the passed name is valid and returns a cleaned copy
     '''
-    # Using a fairly simple regex to make sure it only has letters and spaces
+    # Using a fairly simple regex to make sure it only has alphanumberic
+    # letters and spaces
     # If something more advanced was need I'd go with the following lib or
     # something like it:
     # https://pypi.org/project/nameparser/
-    name = None
-    if value == "":
+    _name = name.strip()
+
+    if _name == "":
         response_body["errors"].append("user name was blank")
         return None
 
     regex = r'([A-Za-z]+)( [A-Za-z]+)*( [A-Za-z]+)*$'
-    name = value.strip()
-    if(re.fullmatch(regex, name)):
-        return name
+    if(re.fullmatch(regex, _name)):
+        return _name
 
     response_body["errors"].append(f"{name}: is not a valid name")
     return None
@@ -161,13 +163,16 @@ def create_user(name, email, manager_id, salary, hire_date):
             "is_active": True,
             "hashed_password": bcrypt.hashpw(b"password", bcrypt.gensalt()),
     }
-    db.user.insert_one(user)
+    # Need to update the chain of command for every new user
+    db.user.insert_one(user).inserted_id
+    update_chain_command.append(user)
 
 
 def update_user(name, email, manager_id, salary, hire_date):
     '''
         Updates user based on the passed values
     '''
+    update = False
     # Name should be valid at this point no matter what
     query_filter = {"normalized_email": email}
     new_values = {"$set": {"name": name}}
@@ -184,6 +189,8 @@ def update_user(name, email, manager_id, salary, hire_date):
     if manager_id is not None:
         if manager_id == "":
             manager_id = None
+        else:
+            update = True
 
         d = new_values["$set"]
         d["manager_id"] = manager_id
@@ -196,6 +203,14 @@ def update_user(name, email, manager_id, salary, hire_date):
         d["hire_date"] = hire_date
 
     db.user.update_one(query_filter, new_values)
+
+    # There was a change in the user's chain of command so we need to update
+    if update:
+        user = {
+            "normalized_email": email,
+            "manager_id": manager_id,
+        }
+        update_chain_command.append(user)
 
 
 def create_chain_of_command(user_email, manager_id):
@@ -213,7 +228,11 @@ def create_chain_of_command(user_email, manager_id):
         "chain_of_command": managers
     }
 
-    db.chain_of_command.insert(chain_of_command)
+    db.chain_of_command.find_one_and_replace(
+        {"user_id": user_id},
+        chain_of_command,
+        upsert=True
+    )
 
 
 def get_chain_of_command(manager_id):
@@ -230,13 +249,16 @@ def get_chain_of_command(manager_id):
         _id = managers_to_search[0]
         del managers_to_search[0]
 
-        manager = db.chain_of_command.find_one({"user_id": _id})
-        if manager is None:
+        _user = db.user.find_one({"_id": _id})
+        if _user is None:
             return result
 
-        for x in manager["chain_of_command"]:
-            managers_to_search.append(x)
-            result.append(x)
+        _manager = _user["manager_id"]
+        if _manager is None:
+            return result
+
+        managers_to_search.append(_manager)
+        result.append(_manager)
 
     return result
 
@@ -280,11 +302,19 @@ def handle_csv_upload(event, context):
     '''
         Transforms CSV data into user's and chain_of_command objects
     '''
+    # Creating this global as I want the functions to handle errors
+    # This allows them to be more detailed for how/why the data is invalid
+    global response_body
     response_body = {
         "numCreated": 0,
         "numUpdated": 0,
         "errors": [],
     }
+
+    # This is used at the end to narrow which users need their
+    # chain of command updated.
+    global update_chain_command
+    update_chain_command = []
 
     # Splitting on newlines to simulate lines
     lines = event.split("\n")
@@ -296,13 +326,12 @@ def handle_csv_upload(event, context):
     reader = csv.DictReader(lines, delimiter=',')
 
     for line in reader:
-
         # The fields below cannot be null
-        name = validate_name(line["Name"], response_body)
-        email = validate_user_email(line["Email"], response_body)
+        name = validate_name(line["Name"])
+        email = validate_user_email(line["Email"])
 
         # The fields below can be null
-        manager_id = process_manager_id(line["Manager"], response_body)
+        manager_id = process_manager_id(line["Manager"])
 
         # If manager Id is None but the email is valid
         # then the id is valid but we didn't find a user
@@ -318,8 +347,8 @@ def handle_csv_upload(event, context):
                                                    + "is not a valid manager "
                                                    + "id.")
 
-        salary = process_salary(line["Salary"], response_body)
-        hire_date = process_hire_date(line["Hire Date"], response_body)
+        salary = process_salary(line["Salary"])
+        hire_date = process_hire_date(line["Hire Date"])
 
         # This check is here to allows us to log errors in user data but not
         # create or update anything due to the name or email not being valid
@@ -341,16 +370,13 @@ def handle_csv_upload(event, context):
 
     # Time to create the chain_of_command after we have ran through the CSV
     # this removes the complexity of having to build and update them as we
-    # create users
-    # This could be optimized by tracking new users and which
-    # existing ones had manager changes
-    users = db.user.find()
+    # create users. We only need to create newly created users or the ones who
+    # had an update to their chain of command
+    for user in update_chain_command:
+        _email = user["normalized_email"]
+        _manager_id = user["manager_id"]
 
-    for user in users:
-        email = user["normalized_email"]
-        manager_id = user["manager_id"]
-        db.chain_of_command.delete_one({"user_id": user["_id"]})
-        create_chain_of_command(email, manager_id)
+        create_chain_of_command(_email, _manager_id)
 
     response = {
         "statusCode": 200,
